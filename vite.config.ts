f19fabbb6aa215b7d43ad5eab27d3d08677559bd
James Carmichael <updownupdown@gmail.com>
2026-04-26 17:46:34 -0400

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/morse/",
   server: {
-    port: 3000, // Match CRA's default port
     open: true,
   },
-  build: {
-    outDir: "build", // Match CRA's build folder name
-  },
-  base: "/morse/",
 });
